107c82efe30e4cf29bd97ede1be840b5f6ff1f2f
Argus74 <57573870+Argus74@users.noreply.github.com>
2025-08-06 05:09:55 -0400

    Created barrel that explodes on contact with projectiles

---
+++ Source/ActionGame/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Barrel = CreateDefaultSubobject<UStaticMeshComponent>("BarrelComp");
+	RootComponent = Barrel;
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>("RadialForceComp");
+	RadialForceComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	Barrel->OnComponentHit.AddDynamic(this,  &ASExplosiveBarrel::Explode);
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void ASExplosiveBarrel::Explode(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComponent->Radius = 2000.0f;
+	RadialForceComponent->ImpulseStrength = 900000.0f;
+	RadialForceComponent->FireImpulse();
+}
+
+
---
+++ Source/ActionGame/Public/SExplosiveBarrel.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+
+UCLASS()
+class ACTIONGAME_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION()
+	void Explode(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+private:
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Barrel;
+
+	URadialForceComponent* RadialForceComponent;
+};
